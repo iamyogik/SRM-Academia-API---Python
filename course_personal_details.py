da5a0b29f2fefdb076558c5e231f1091565a1a78
harshithmohan <gharshitmohan@ymail.com>
2019-02-26 00:43:02 +0530

    Personal info. fixed

--- course_personal_details.py
+++ course_personal_details.py
@@ -65,9 +65,9 @@ url = "https://academia.srmuniv.ac.in/liveViewHeader.do"
 
 def getCoursePersonalDetailsData(token,sem="ODD"):
     if(sem == "EVEN"):
-        viewLinkName = "My_Time_Table_2018_19_EVEN"
+        viewLinkName = "My_Time_Table_2018_19_Even"
     elif(sem == "ODD"):
-        viewLinkName = "My_Time_Table_2018_19_ODD"
+        viewLinkName = "My_Time_Table_2018_19_Odd"
     else:
         json_o = {"status": "error", "msg": "Error in batch name."}
         json_o = json.dumps(json_o)
@@ -97,13 +97,6 @@ def getCoursePersonalDetailsData(token,sem="ODD"):
         a, b = dom.find(s1), dom.find(s2)
         dom = pq(dom[a + 56 + len(viewLinkName):b - 5])
 
-        print(dom)
-
-        json_o = {"status": "success", "msg": "Error occured"}
-        json_o = json.dumps(json_o)
-        return json_o
-
-
         dom('table[border="1"]').find('tr:nth-child(n + 2)').each(get_CourseDetails)
         dom('td[align="center"]').each(get_facultyadvisordetails)
 
@@ -120,19 +113,19 @@ def getCoursePersonalDetailsData(token,sem="ODD"):
 
 def getCoursePersonalDetails(token):
     CompleteDetails = getCoursePersonalDetailsData(token, "EVEN")
-    # if len(CompleteDetails['PersonalDetails']['RegistrationNumber']) > 5:
-    #     json_o = {"status": "success", "data": CompleteDetails}
-    #     json_o = json.dumps(json_o)
-    #     return json_o
-    # else:
-    #     CompleteDetails2 = getCoursePersonalDetailsData(token, "ODD")
-    #     if len(CompleteDetails2['PersonalDetails']['RegistrationNumber']) > 5:
-    #         json_o = {"status": "success", "data": CompleteDetails2}
-    #         json_o = json.dumps(json_o)
-    #         return json_o
-    #     else:
-    #         json_o = {"status": "error", "msg": "Error occured"}
-    #         json_o = json.dumps(json_o)
-    #         return json_o
+    if len(CompleteDetails['PersonalDetails']['RegistrationNumber']) > 5:
+        json_o = {"status": "success", "data": CompleteDetails}
+        json_o = json.dumps(json_o)
+        return json_o
+    else:
+        CompleteDetails2 = getCoursePersonalDetailsData(token, "ODD")
+        if len(CompleteDetails2['PersonalDetails']['RegistrationNumber']) > 5:
+            json_o = {"status": "success", "data": CompleteDetails2}
+            json_o = json.dumps(json_o)
+            return json_o
+        else:
+            json_o = {"status": "error", "msg": "Error occured"}
+            json_o = json.dumps(json_o)
+            return json_o
 
     
